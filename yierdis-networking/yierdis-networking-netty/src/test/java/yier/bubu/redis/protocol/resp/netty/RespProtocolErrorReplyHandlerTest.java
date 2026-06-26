23fd12f135acad27a5dcacb18ec0787c0f17bca7
bubu <1654388696@qq.com>
2026-06-26 16:00:09 +0800

    test: tighten protocol error handler closing coverage

--- yierdis-networking/yierdis-networking-netty/src/test/java/yier/bubu/redis/protocol/resp/netty/RespProtocolErrorReplyHandlerTest.java
+++ yierdis-networking/yierdis-networking-netty/src/test/java/yier/bubu/redis/protocol/resp/netty/RespProtocolErrorReplyHandlerTest.java
@@ -1,19 +1,13 @@
 package yier.bubu.redis.protocol.resp.netty;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.DefaultChannelPromise;
 import io.netty.channel.embedded.EmbeddedChannel;
-import io.netty.util.concurrent.ImmediateEventExecutor;
 import org.junit.Assert;
 import org.junit.Test;
 import yier.bubu.redis.protocol.resp.RespReplyWriterFactory;
 
-import java.lang.reflect.Proxy;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -47,20 +41,26 @@ public class RespProtocolErrorReplyHandlerTest {
     public void closeAfterReplyDropsLaterInboundMessagesOnceClosingStarts() {
         AtomicInteger downstreamReads = new AtomicInteger();
         TrackingCloseable laterInbound = new TrackingCloseable();
-        RespProtocolErrorReplyHandler handler = new RespProtocolErrorReplyHandler(new RespReplyWriterFactory());
-        TrackingChannelHandlerContext ctx = new TrackingChannelHandlerContext(downstreamReads);
+        EmbeddedChannel ch = new EmbeddedChannel(
+                new RespProtocolErrorReplyHandler(
+                        new RespReplyWriterFactory(),
+                        ctx -> ctx.pipeline().fireChannelRead(laterInbound)
+                ),
+                new ChannelInboundHandlerAdapter() {
+                    @Override
+                    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                        downstreamReads.incrementAndGet();
+                    }
+                }
+        );
         try {
-            handler.channelRead(ctx.proxy(), new RespProtocolError("ERR Protocol error", true));
-            handler.channelRead(ctx.proxy(), laterInbound);
-
-            Assert.assertFalse(ctx.channel().config().isAutoRead());
+            Assert.assertFalse(ch.writeInbound(new RespProtocolError("ERR Protocol error", true)));
             Assert.assertTrue(laterInbound.closed);
             Assert.assertEquals(0, downstreamReads.get());
-            Assert.assertArrayEquals(ascii("-ERR Protocol error\r\n"), ctx.writtenBytes);
-        } catch (Exception e) {
-            throw new AssertionError(e);
+            Assert.assertArrayEquals(ascii("-ERR Protocol error\r\n"), readOutbound(ch));
+            Assert.assertFalse(ch.isOpen());
         } finally {
-            ctx.channel().finishAndReleaseAll();
+            ch.finishAndReleaseAll();
         }
     }
 
@@ -88,66 +88,6 @@ public class RespProtocolErrorReplyHandlerTest {
         @Override
         public void close() {
             closed = true;
-        }
-    }
-
-    private static final class TrackingChannelHandlerContext {
-        private final EmbeddedChannel channel = new EmbeddedChannel();
-        private final AtomicInteger downstreamReads;
-        private final ChannelHandlerContext proxy;
-        private byte[] writtenBytes;
-
-        private TrackingChannelHandlerContext(AtomicInteger downstreamReads) {
-            this.downstreamReads = downstreamReads;
-            this.proxy = (ChannelHandlerContext) Proxy.newProxyInstance(
-                    ChannelHandlerContext.class.getClassLoader(),
-                    new Class<?>[]{ChannelHandlerContext.class},
-                    (obj, method, args) -> switch (method.getName()) {
-                        case "channel" -> channel;
-                        case "alloc" -> ByteBufAllocator.DEFAULT;
-                        case "executor" -> ImmediateEventExecutor.INSTANCE;
-                        case "name" -> "tracking";
-                        case "handler" -> null;
-                        case "isRemoved" -> false;
-                        case "fireChannelRead" -> {
-                            downstreamReads.incrementAndGet();
-                            yield obj;
-                        }
-                        case "writeAndFlush", "write" -> writeAndFlush(args[0]);
-                        case "newPromise" -> new DefaultChannelPromise(channel, ImmediateEventExecutor.INSTANCE);
-                        case "voidPromise" -> channel.voidPromise();
-                        case "pipeline" -> channel.pipeline();
-                        case "flush", "read", "fireChannelRegistered", "fireChannelUnregistered", "fireChannelActive",
-                             "fireChannelInactive", "fireExceptionCaught", "fireUserEventTriggered",
-                             "fireChannelReadComplete", "fireChannelWritabilityChanged" -> obj;
-                        case "attr" -> channel.attr((io.netty.util.AttributeKey<Object>) args[0]);
-                        case "hasAttr" -> channel.hasAttr((io.netty.util.AttributeKey<Object>) args[0]);
-                        case "toString" -> "TrackingChannelHandlerContext";
-                        case "hashCode" -> System.identityHashCode(obj);
-                        case "equals" -> obj == args[0];
-                        default -> throw new UnsupportedOperationException(method.toString());
-                    }
-            );
-        }
-
-        private ChannelHandlerContext proxy() {
-            return proxy;
-        }
-
-        private EmbeddedChannel channel() {
-            return channel;
-        }
-
-        private ChannelFuture writeAndFlush(Object msg) {
-            if (msg instanceof ByteBuf out) {
-                try {
-                    writtenBytes = new byte[out.readableBytes()];
-                    out.getBytes(out.readerIndex(), writtenBytes);
-                } finally {
-                    out.release();
-                }
-            }
-            return new DefaultChannelPromise(channel, ImmediateEventExecutor.INSTANCE);
         }
     }
 }
